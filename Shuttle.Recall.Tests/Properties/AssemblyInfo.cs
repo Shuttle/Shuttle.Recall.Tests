9e6d2ce1fe7391907b7ae6351a821901c71eebbc
Eben <me@ebenroux.co.za>
2015-11-06 09:28:52 +0200

    - v1.0.1

--- Shuttle.Recall.Tests/Properties/AssemblyInfo.cs
+++ Shuttle.Recall.Tests/Properties/AssemblyInfo.cs
@@ -1,36 +1,23 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Shuttle.Recall.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Shuttle.Recall.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+#if NET40FULL
+[assembly: AssemblyTitle("Shuttle.Recall.Tests for .NET Framework 4.0")]
+#endif
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+#if NET45FULL
+[assembly: AssemblyTitle("Shuttle.Recall.Tests for .NET Framework 4.5")]
+#endif
+
+#if NET451FULL
+[assembly: AssemblyTitle("Shuttle.Recall.Tests for .NET Framework 4.5.1")]
+#endif
+
+[assembly: AssemblyVersion("3.6.0.0")]
+[assembly: AssemblyCopyright("Copyright © Eben Roux 2010-2015")]
+[assembly: AssemblyProduct("Shuttle.Recall")]
+[assembly: AssemblyCompany("Shuttle")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyInformationalVersion("3.6.0")]
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("da9a3ed3-7584-4c35-95f7-44958edf7be5")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
