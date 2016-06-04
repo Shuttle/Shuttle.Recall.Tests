65cb84d9439b70d895b8ef0ac2470911c1eddd56
Eben <me@ebenroux.co.za>
2016-06-04 14:53:48 +0200

    - v1.7.1

--- Shuttle.Recall.Tests/Properties/AssemblyInfo.cs
+++ Shuttle.Recall.Tests/Properties/AssemblyInfo.cs
@@ -26,10 +26,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Shuttle.Recall.Tests for .NET Framework 4.6.1")]
 #endif
 
-[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyVersion("1.7.1.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2010-2015")]
 [assembly: AssemblyProduct("Shuttle.Recall")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("1.7.0")]
+[assembly: AssemblyInformationalVersion("1.7.1")]
 [assembly: ComVisible(false)]
