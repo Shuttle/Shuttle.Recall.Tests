4413a0c8c60bf37fc6c1b8477ff94a17c44dfef7
Eben <me@ebenroux.co.za>
2019-05-27 17:43:03 +0200

    - v10.2.1

--- Shuttle.Recall.Tests/Properties/AssemblyInfo.cs
+++ Shuttle.Recall.Tests/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 #endif
 
 [assembly: AssemblyVersion("10.2.1.0")]
-[assembly: AssemblyCopyright("Copyright © Eben Roux 2018")]
+[assembly: AssemblyCopyright("Copyright © Eben Roux 2019")]
 [assembly: AssemblyProduct("Shuttle.Recall.Tests")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
