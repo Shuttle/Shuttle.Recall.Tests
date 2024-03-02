0a999af9793751ef458ca9765d794f60db66f238
Eben <me@ebenroux.co.za>
2024-03-02 20:07:28 +0200

    - split event store test and event processing test

--- Shuttle.Recall.Tests.Memory/MemoryFixture.cs
+++ Shuttle.Recall.Tests.Memory/MemoryFixture.cs
@@ -10,18 +10,18 @@ namespace Shuttle.Recall.Tests.Memory
     public class MemoryFixture : RecallFixture
     {
         [Test]
-        public void Should_be_able_to_exercise_event_store_and_processing()
+        public void Should_be_able_to_exercise_event_store()
         {
-            Should_be_able_to_exercise_event_store_and_processing_async(true).GetAwaiter().GetResult();
+            Should_be_able_to_exercise_event_store_async(true).GetAwaiter().GetResult();
         }
 
         [Test]
-        public async Task Should_be_able_to_exercise_event_store_and_processing_async()
+        public async Task Should_be_able_to_exercise_event_store_async()
         {
-            await Should_be_able_to_exercise_event_store_and_processing_async(false).ConfigureAwait(false);
+            await Should_be_able_to_exercise_event_store_async(false).ConfigureAwait(false);
         }
 
-        private async Task Should_be_able_to_exercise_event_store_and_processing_async(bool sync)
+        private async Task Should_be_able_to_exercise_event_store_async(bool sync)
         {
             var services = new ServiceCollection();
 
@@ -39,14 +39,49 @@ namespace Shuttle.Recall.Tests.Memory
             if (sync)
             {
                 ExerciseStorage(eventStore);
-                ExerciseEventProcessing(serviceProvider.GetRequiredService<IEventProcessor>(), 60);
                 ExerciseStorageRemoval(eventStore);
             }
             else
             {
                 await ExerciseStorageAsync(eventStore);
-                await ExerciseEventProcessingAsync(serviceProvider.GetRequiredService<IEventProcessor>(), 60);
                 await ExerciseStorageRemovalAsync(eventStore);
+            }
+        }
+
+        [Test]
+        public void Should_be_able_to_exercise_event_processing()
+        {
+            Should_be_able_to_exercise_event_processing_async(true).GetAwaiter().GetResult();
+        }
+
+        [Test]
+        public async Task Should_be_able_to_exercise_event_processing_async()
+        {
+            await Should_be_able_to_exercise_event_processing_async(false).ConfigureAwait(false);
+        }
+
+        private async Task Should_be_able_to_exercise_event_processing_async(bool sync)
+        {
+            var services = new ServiceCollection();
+
+            var store = new Dictionary<Guid, List<PrimitiveEvent>>();
+
+            services.AddSingleton<IProjectionRepository, MemoryProjectionRepository>();
+            services.AddSingleton<IPrimitiveEventRepository>(new MemoryPrimitiveEventRepository(store));
+            services.AddSingleton<IPrimitiveEventQuery>(new MemoryPrimitiveEventQuery(store));
+            services.AddEventStore(builder => builder.Options.Asynchronous = !sync);
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var eventStore = serviceProvider.GetRequiredService<IEventStore>();
+
+            if (sync)
+            {
+                ExerciseEventProcessing(eventStore, serviceProvider.GetRequiredService<IEventProcessor>(), 60);
+            }
+            else
+            {
+                await ExerciseEventProcessingAsync(eventStore, serviceProvider.GetRequiredService<IEventProcessor>(), 60);
             }
         }
     }
--- Shuttle.Recall.Tests/RecallFixture.cs
+++ Shuttle.Recall.Tests/RecallFixture.cs
@@ -11,19 +11,29 @@ namespace Shuttle.Recall.Tests
         public static readonly Guid OrderId = new Guid("047FF6FB-FB57-4F63-8795-99F252EDA62F");
         public static readonly Guid OrderProcessId = new Guid("74937207-F430-4746-9F31-4E76EF2FA7E6");
 
-        public void ExerciseEventProcessing(IEventProcessor processor, int handlerTimeoutSeconds = 5)
+        public void ExerciseEventProcessing(IEventStore eventStore, IEventProcessor processor, int handlerTimeoutSeconds = 5)
         {
-            ExerciseEventProcessingAsync(processor, handlerTimeoutSeconds, true).GetAwaiter().GetResult();
+            ExerciseEventProcessingAsync(eventStore, processor, handlerTimeoutSeconds, true).GetAwaiter().GetResult();
         }
 
-        public async Task ExerciseEventProcessingAsync(IEventProcessor processor, int handlerTimeoutSeconds = 5)
+        public async Task ExerciseEventProcessingAsync(IEventStore eventStore, IEventProcessor processor, int handlerTimeoutSeconds = 5)
         {
-            await ExerciseEventProcessingAsync(processor, handlerTimeoutSeconds, false).ConfigureAwait(false);
+            await ExerciseEventProcessingAsync(eventStore, processor, handlerTimeoutSeconds, false).ConfigureAwait(false);
         }
 
-        private async Task ExerciseEventProcessingAsync(IEventProcessor processor, int handlerTimeoutSeconds, bool sync)
+        private async Task ExerciseEventProcessingAsync(IEventStore eventStore, IEventProcessor processor, int handlerTimeoutSeconds, bool sync)
         {
+            Guard.AgainstNull(eventStore, nameof(eventStore));
             Guard.AgainstNull(processor, nameof(processor));
+
+            if (sync)
+            {
+                ExerciseStorage(eventStore);
+            }
+            else
+            {
+                await ExerciseStorageAsync(eventStore).ConfigureAwait(false);
+            }
 
             var handler = new OrderHandler();
 
@@ -64,27 +74,36 @@ namespace Shuttle.Recall.Tests
             }
 
             Assert.IsFalse(handler.HasTimedOut, "The handler has timed out.  Not all of the events have been processed by the projection.");
+
+            if (sync)
+            {
+                ExerciseStorageRemoval(eventStore);
+            }
+            else
+            {
+                await ExerciseStorageRemovalAsync(eventStore).ConfigureAwait(false);
+            }
         }
 
-        public void ExerciseStorage(IEventStore store)
+        public void ExerciseStorage(IEventStore eventStore)
         {
-            ExerciseStorageAsync(store, true).GetAwaiter().GetResult();
+            ExerciseStorageAsync(eventStore, true).GetAwaiter().GetResult();
         }
 
-        public async Task ExerciseStorageAsync(IEventStore store)
+        public async Task ExerciseStorageAsync(IEventStore eventStore)
         {
-            await ExerciseStorageAsync(store, false).ConfigureAwait(false);
+            await ExerciseStorageAsync(eventStore, false).ConfigureAwait(false);
         }
 
-        private async Task ExerciseStorageAsync(IEventStore store, bool sync)
+        private async Task ExerciseStorageAsync(IEventStore eventStore, bool sync)
         {
-            Guard.AgainstNull(store, nameof(store));
+            Guard.AgainstNull(eventStore, nameof(eventStore));
 
             var order = new Order(OrderId);
             var orderProcess = new OrderProcess(OrderProcessId);
 
-            var orderStream = sync ? store.Get(OrderId) : await store.GetAsync(OrderId).ConfigureAwait(false);
-            var orderProcessStream = sync ? store.Get(OrderProcessId) : await store.GetAsync(OrderProcessId).ConfigureAwait(false);
+            var orderStream = sync ? eventStore.Get(OrderId) : await eventStore.GetAsync(OrderId).ConfigureAwait(false);
+            var orderProcessStream = sync ? eventStore.Get(OrderProcessId) : await eventStore.GetAsync(OrderProcessId).ConfigureAwait(false);
 
             orderStream.AddEvent(order.AddItem("t-shirt", 5, 125));
             orderStream.AddEvent(order.AddItem("baked beans", 2, 4.55));
@@ -94,26 +113,26 @@ namespace Shuttle.Recall.Tests
 
             if (sync)
             {
-                store.Save(orderStream);
+                eventStore.Save(orderStream);
             }
             else
             {
-                await store.SaveAsync(orderStream).ConfigureAwait(false);
+                await eventStore.SaveAsync(orderStream).ConfigureAwait(false);
             }
 
             orderProcessStream.AddEvent(orderProcess.StartPicking());
 
             if (sync)
             {
-                store.Save(orderProcessStream);
+                eventStore.Save(orderProcessStream);
             }
             else
             {
-                await store.SaveAsync(orderProcessStream).ConfigureAwait(false);
+                await eventStore.SaveAsync(orderProcessStream).ConfigureAwait(false);
             }
 
             order = new Order(OrderId);
-            orderStream = sync ? store.Get(OrderId) : await store.GetAsync(OrderId).ConfigureAwait(false);
+            orderStream = sync ? eventStore.Get(OrderId) : await eventStore.GetAsync(OrderId).ConfigureAwait(false);
 
             orderStream.Apply(order);
 
@@ -123,15 +142,15 @@ namespace Shuttle.Recall.Tests
 
             if (sync)
             {
-                store.Save(orderStream);
+                eventStore.Save(orderStream);
             }
             else
             {
-                await store.SaveAsync(orderStream).ConfigureAwait(false);
+                await eventStore.SaveAsync(orderStream).ConfigureAwait(false);
             }
 
             orderProcess = new OrderProcess(OrderProcessId);
-            orderProcessStream = sync ? store.Get(OrderProcessId) : await store.GetAsync(OrderProcessId).ConfigureAwait(false);
+            orderProcessStream = sync ? eventStore.Get(OrderProcessId) : await eventStore.GetAsync(OrderProcessId).ConfigureAwait(false);
             orderProcessStream.Apply(orderProcess);
 
             Assert.IsTrue(orderProcess.CanChangeStatusTo(OrderProcessStatus.Fulfilled), "Should be able to change status to 'Fulfilled'");
@@ -142,32 +161,32 @@ namespace Shuttle.Recall.Tests
 
             if (sync)
             {
-                store.Save(orderStream);
+                eventStore.Save(orderStream);
             }
             else
             {
-                await store.SaveAsync(orderStream).ConfigureAwait(false);
+                await eventStore.SaveAsync(orderStream).ConfigureAwait(false);
             }
 
             orderProcessStream.AddEvent(orderProcess.Fulfill());
 
             if (sync)
             {
-                store.Save(orderProcessStream);
+                eventStore.Save(orderProcessStream);
             }
             else
             {
-                await store.SaveAsync(orderProcessStream).ConfigureAwait(false);
+                await eventStore.SaveAsync(orderProcessStream).ConfigureAwait(false);
             }
 
             order = new Order(OrderId);
-            orderStream = sync ? store.Get(OrderId) : await store.GetAsync(OrderId).ConfigureAwait(false);
+            orderStream = sync ? eventStore.Get(OrderId) : await eventStore.GetAsync(OrderId).ConfigureAwait(false);
             orderStream.Apply(order);
 
             Assert.AreEqual(orderTotal, order.Total(), "The total of the second re-constituted order does not equal the expected amount of '{0}'.", orderTotal);
 
             orderProcess = new OrderProcess(OrderProcessId);
-            orderProcessStream = sync ? store.Get(OrderProcessId) : await store.GetAsync(OrderProcessId);
+            orderProcessStream = sync ? eventStore.Get(OrderProcessId) : await eventStore.GetAsync(OrderProcessId);
             orderProcessStream.Apply(orderProcess);
 
             Assert.IsFalse(orderProcess.CanChangeStatusTo(OrderProcessStatus.Fulfilled), "Should not be able to change status to 'Fulfilled'");
